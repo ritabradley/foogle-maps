86e80f0e6e1daa01aa8866d225bc7e88dc5bbded
Rita Bradley <contact@ritabradley.com>
2020-05-30 20:31:06 -0700

    feat: Add new classes and render map
    - Add User, Company, and Map classes
    - Load Google Maps API
    - Add faker and Google Maps type definitions

---
+++ src/components/Company.ts
@@ -0,0 +1,19 @@
+import faker from 'faker';
+
+export class Company {
+    constructor() {
+        this.name = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude()),
+        };
+    }
+
+    name: string;
+    catchPhrase: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { User } from './components/User';
+import { Company } from './components/Company';
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0,
+    },
+});
